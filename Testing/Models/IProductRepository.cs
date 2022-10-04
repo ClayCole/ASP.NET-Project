19a99c461f20ace90905704a48c98fcb9e328475
Clay Coleman <claycoleman94@gamil.com>
2022-10-04 18:00:10 -0400

    Update

---
+++ Testing/Models/IProductRepository.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Testing.Models
+{
+    public interface IProductRepository
+    {
+        public IEnumerable<Product> GetAllProducts();
+        public Product GetProduct(int id);
+        public void UpdateProduct(Product product);
+
+        public void InsertProduct(Product productToInsert); // AssignCategory()
+        public IEnumerable<Category> GetCategories(); // GetCategories()
+        public Product AssignCategory(); // InsertProduct()
+
+        public void DeleteProduct(Product product);
+
+    }
+}
+
